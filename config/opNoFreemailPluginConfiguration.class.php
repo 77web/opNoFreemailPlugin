2c3e07bfc93e61408cf52843947ab191a17850fa
77web <info@77-web.com>
2012-06-11 20:39:39 +0900

    登録成立後のフリーメールへの変更も抑制するように。実装状況更新。

--- config/opNoFreemailPluginConfiguration.class.php
+++ config/opNoFreemailPluginConfiguration.class.php
@@ -10,10 +10,15 @@ class opNoFreemailPluginConfiguration extends sfPluginConfiguration
   public function mergeNoFreemailValidator(sfEvent $event)
   {
     $form = $event->getSubject();
+    $formName = get_class($form);
     
-    if ('opRequestRegisterURLForm' == get_class($form))
+    if ('opRequestRegisterURLForm' == $formName)
     {
       $form->mergePostValidator(new sfValidatorCallback(array('callback' => array($this, 'isNotFreemail'))));
+    }
+    elseif ('MemberConfigPcAddressForm' == $formName)
+    {
+      $form->mergePostValidator(new sfValidatorCallback(array('callback' => array($this, 'isNotChangingToFreemail'))));
     }
   }
   
@@ -30,6 +35,25 @@ class opNoFreemailPluginConfiguration extends sfPluginConfiguration
       catch (sfValidatorError $e)
       {
         throw new sfValidatorErrorSchema($validator, array('mail_address' => $e));
+      }
+    }
+    
+    return $values;
+  }
+  
+  public function isNotChangingToFreemail($validator, $values, $arguments = array())
+  {
+    if (isset($values['pc_address']))
+    {
+      $noFreemailValidator = new opValidatorNoFreemail();
+      
+      try
+      {
+        $noFreemailValidator->clean($values['pc_address']);
+      }
+      catch (sfValidatorError $e)
+      {
+        throw new sfValidatorErrorSchema($validator, array('pc_address' => $e));
       }
     }
     
---
+++ test/functional/pc_frontend/memberConfigPcAddressActionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+include dirname(__FILE__).'/../../bootstrap/functional.php';
+include dirname(__FILE__).'/../../bootstrap/database.php';
+
+$browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
+$browser->setCulture('en');
+
+$browser->login('sns@example.com', 'password')->with('user')->isAuthenticated()->setCulture('en');
+sfConfig::set('op_is_use_captcha', false);
+
+$browser
+  ->get('/member/config/category/pcAddress')
+    ->with('request')->begin()
+      ->isParameter('module', 'member')
+      ->isParameter('action', 'config')
+      ->isParameter('category', 'pcAddress')
+    ->end()
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->checkElement('#pcAddressForm')
+    ->end()
+    ->setField('member_config[pc_address]', 'user@yahoo.co.jp')
+    ->click('Send')
+    
+    ->with('request')->begin()
+      ->isParameter('module', 'member')
+      ->isParameter('action', 'config')
+      ->isParameter('category', 'pcAddress')
+    ->end()
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->checkElement('.error_list li:contains("Free mail is not allowed.")')
+    ->end()
+;
